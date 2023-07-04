5d4e49e163eef1e50d5cc6bd6c6f6e0c0e2f4cde
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-07-04 13:46:25 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -9,6 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
 
             var pwd = "mypasswordis12345";
+            var secret_key = "445675-3456-433246-23567-34566"
 
             Console.WriteLine(pwd);
             
