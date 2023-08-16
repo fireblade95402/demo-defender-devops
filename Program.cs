93db30a167f5dbd175dbd03d51885f1554c87d94
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-08-16 10:41:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
-﻿            Console.WriteLine("Hello World!!!");
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
-            string pwd;
-            string key;
-            string secret;
-            string grafana_cloud_api_token;
+namespace SecretAlert
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Create a HttpClient object
+            var client = new HttpClient();
 
-            string client_id = "c53fa4fa-1c56-40a5-8a41-11be70b2337d";
-            string client_secret = "ejr8Q~pyH98kCz4gWW5diHZ42GFe~gFD.XU36dAU";
-            
-            key="pass1234";
-            pwd="pass1234";
-            secret="test12233pwd123";
-            grafana_cloud_api_token = "nL540gX2BnSlu-b47QExPOizRdDHD8BFv-PDbahFe7yoAzFuCFyUae==";    
+            var client_id = "c53fa4fa-1c56-40a5-8a41-11be70b2337d";
+            var client_secret = "ejr8Q~pyH98kCz4gWW5diHZ42GFe~gFD.XU36dAU";
 
-            Console.WriteLine("Great Code, NOT!");
+            // Set the request headers
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", client_id);
+            client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
+
+            // Set the request URL
+            var url = "https://api.github.com/orgs/ORG/actions/secrets/client_secret";
+
+            // Send a DELETE request
+            var response = await client.DeleteAsync(url);
+
+            // Print the response status code
+            Console.WriteLine(response.StatusCode);
+        }
+    }
+}
+
