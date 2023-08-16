8e11cdc691e938cd4f95f9e8319bc40dd9299882
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-08-16 10:19:15 +0100

    test

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@
             string secret;
             string grafana_cloud_api_token;
 
-            string client_id = "c53fa4fa-1c56-40a5-8a41-11be70b2337d"
-            string client_secret = "ejr8Q~pyH98kCz4gWW5diHZ42GFe~gFD.XU36dAU"
+            string client_id = "c53fa4fa-1c56-40a5-8a41-11be70b2337d";
+            string client_secret = "ejr8Q~pyH98kCz4gWW5diHZ42GFe~gFD.XU36dAU";
             
             key="pass1234";
             pwd="pass1234";
