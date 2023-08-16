ef97193abad59d3de6dcad942bb06bf4478f861b
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-08-16 09:16:27 +0100

    Update main.cs

--- main.cs
+++ main.cs
@@ -15,6 +15,7 @@ namespace HelloWorld
             key="pass1234";
             pwd="pass1234";
             secret="test12233pwd123";
+            strsecretKey="nL540gX2BnSlu-b47QExPOizRdDHD8BFv-PDbahFe7yoAzFuCFyUag=="
                 
 
             Console.WriteLine("Great Code, NOT!);
