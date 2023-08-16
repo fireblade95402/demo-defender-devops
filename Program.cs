fda587eaf80fc31427788a2d8d5c3af1bcbad4c1
Mark <34512169+fireblade95402@users.noreply.github.com>
2023-08-16 09:52:40 +0100

    update

--- Program.cs
+++ Program.cs
@@ -1,24 +1,13 @@
-using System;
-
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+﻿            Console.WriteLine("Hello World!");
 
             string pwd;
             string key;
             string secret;
+            string grafana_cloud_api_token;
             
             key="pass1234";
             pwd="pass1234";
             secret="test12233pwd123";
             grafana_cloud_api_token = "nL540gX2BnSlu-b47QExPOizRdDHD8BFv-PDbahFe7yoAzFuCFyUae==";    
 
-            Console.WriteLine("Great Code, NOT!);
-            
-        }
-    }
-}
+            Console.WriteLine("Great Code, NOT!");
